9c742d91415a4bf4868d21db0b8febe86d1838a0
surajv1986 <surajv1986@gmail.com>
2023-11-14 19:31:30 +0530

    Added Mutex APIs

--- FreeRTOS_Port/Debug/Core/Src/subdir.mk
+++ FreeRTOS_Port/Debug/Core/Src/subdir.mk
@@ -5,6 +5,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../Core/Src/freertos_mutex.c \
 ../Core/Src/freertos_utilities.c \
 ../Core/Src/main.c \
 ../Core/Src/queue.c \
@@ -17,6 +18,7 @@ C_SRCS += \
 ../Core/Src/task_notifications.c 
 
 OBJS += \
+./Core/Src/freertos_mutex.o \
 ./Core/Src/freertos_utilities.o \
 ./Core/Src/main.o \
 ./Core/Src/queue.o \
@@ -29,6 +31,7 @@ OBJS += \
 ./Core/Src/task_notifications.o 
 
 C_DEPS += \
+./Core/Src/freertos_mutex.d \
 ./Core/Src/freertos_utilities.d \
 ./Core/Src/main.d \
 ./Core/Src/queue.d \
@@ -48,7 +51,7 @@ Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/freertos_utilities.cyclo ./Core/Src/freertos_utilities.d ./Core/Src/freertos_utilities.o ./Core/Src/freertos_utilities.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/queue.cyclo ./Core/Src/queue.d ./Core/Src/queue.o ./Core/Src/queue.su ./Core/Src/stm32f4xx_hal_msp.cyclo ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_msp.su ./Core/Src/stm32f4xx_hal_timebase_tim.cyclo ./Core/Src/stm32f4xx_hal_timebase_tim.d ./Core/Src/stm32f4xx_hal_timebase_tim.o ./Core/Src/stm32f4xx_hal_timebase_tim.su ./Core/Src/stm32f4xx_it.cyclo ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/stm32f4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.cyclo ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su ./Core/Src/task_notifications.cyclo ./Core/Src/task_notifications.d ./Core/Src/task_notifications.o ./Core/Src/task_notifications.su
+	-$(RM) ./Core/Src/freertos_mutex.cyclo ./Core/Src/freertos_mutex.d ./Core/Src/freertos_mutex.o ./Core/Src/freertos_mutex.su ./Core/Src/freertos_utilities.cyclo ./Core/Src/freertos_utilities.d ./Core/Src/freertos_utilities.o ./Core/Src/freertos_utilities.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/queue.cyclo ./Core/Src/queue.d ./Core/Src/queue.o ./Core/Src/queue.su ./Core/Src/stm32f4xx_hal_msp.cyclo ./Core/Src/stm32f4xx_hal_msp.d ./Core/Src/stm32f4xx_hal_msp.o ./Core/Src/stm32f4xx_hal_msp.su ./Core/Src/stm32f4xx_hal_timebase_tim.cyclo ./Core/Src/stm32f4xx_hal_timebase_tim.d ./Core/Src/stm32f4xx_hal_timebase_tim.o ./Core/Src/stm32f4xx_hal_timebase_tim.su ./Core/Src/stm32f4xx_it.cyclo ./Core/Src/stm32f4xx_it.d ./Core/Src/stm32f4xx_it.o ./Core/Src/stm32f4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32f4xx.cyclo ./Core/Src/system_stm32f4xx.d ./Core/Src/system_stm32f4xx.o ./Core/Src/system_stm32f4xx.su ./Core/Src/task_notifications.cyclo ./Core/Src/task_notifications.d ./Core/Src/task_notifications.o ./Core/Src/task_notifications.su
 
 .PHONY: clean-Core-2f-Src
 
